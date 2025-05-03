a769a83a138f1b1a64f039871f7eba3539d89a7d
JL <115737916+yikesboy@users.noreply.github.com>
2025-05-03 20:01:40 +0200

    added first plugin

---
+++ lua/custom/plugins/cord-nvim.lua
@@ -0,0 +1,121 @@
+return {
+  'vyfor/cord.nvim',
+  build = ':Cord update',
+  opts = {
+    enabled = true,
+    log_level = vim.log.levels.OFF,
+    editor = {
+      client = 'neovim',
+      tooltip = 'neovim btw...',
+      icon = nil,
+    },
+    display = {
+      theme = 'catppuccin',
+      flavor = 'dark',
+      swap_fields = false,
+      swap_icons = false,
+    },
+    timestamp = {
+      enabled = true,
+      reset_on_idle = false,
+      reset_on_change = false,
+    },
+    idle = {
+      enabled = true,
+      timeout = 300000,
+      show_status = true,
+      ignore_focus = true,
+      unidle_on_focus = true,
+      smart_idle = true,
+      details = 'Idling',
+      state = nil,
+      tooltip = '💤',
+      icon = nil,
+    },
+    text = {
+      default = nil,
+      workspace = function()
+        return ''
+      end,
+      viewing = function()
+        return 'Viewing'
+      end,
+      editing = function()
+        return 'Editing'
+      end,
+      file_browser = function()
+        return 'Browsing files'
+      end,
+      plugin_manager = function()
+        return 'Managing plugins'
+      end,
+      lsp = function()
+        return 'Configuring LSP'
+      end,
+      docs = function()
+        return 'Reading'
+      end,
+      vcs = function()
+        return 'Committing changes'
+      end,
+      notes = function()
+        return 'Taking notes'
+      end,
+      debug = function()
+        return 'Debugging'
+      end,
+      test = function()
+        return 'Testing'
+      end,
+      diagnostics = function()
+        return 'Fixing problems'
+      end,
+      games = function()
+        return 'Playing'
+      end,
+      terminal = function()
+        return 'Running commands'
+      end,
+      dashboard = 'Home',
+    },
+    buttons = nil,
+    assets = nil,
+    variables = nil,
+    hooks = {
+      ready = nil,
+      shutdown = nil,
+      pre_activity = nil,
+      post_activity = nil,
+      idle_enter = nil,
+      idle_leave = nil,
+      workspace_change = nil,
+    },
+    plugins = nil,
+    advanced = {
+      plugin = {
+        autocmds = true,
+        cursor_update = 'on_hold',
+        match_in_mappings = true,
+      },
+      server = {
+        update = 'fetch',
+        pipe_path = nil,
+        executable_path = nil,
+        timeout = 300000,
+      },
+      discord = {
+        reconnect = {
+          enabled = false,
+          interval = 5000,
+          initial = true,
+        },
+      },
+      workspace = {
+        root_markers = {
+          '.git',
+        },
+        limit_to_cwd = false,
+      },
+    },
+  },
+}
