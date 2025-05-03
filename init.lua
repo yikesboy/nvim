75a87faa18abe3d8930f9c712ad4672f7984f224
JL <115737916+yikesboy@users.noreply.github.com>
2025-05-03 23:22:32 +0200

    structure change

--- init.lua
+++ init.lua
@@ -1,78 +1,79 @@
+require 'config.options'
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
--- vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
---vim.schedule(function()
---  vim.opt.clipboard = 'unnamedplus'
---end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
-
+--vim.g.mapleader = ' '
+--vim.g.maplocalleader = ' '
+--
+---- Set to true if you have a Nerd Font installed and selected in the terminal
+--vim.g.have_nerd_font = true
+--
+---- [[ Setting options ]]
+---- See `:help vim.opt`
+---- NOTE: You can change these options as you wish!
+----  For more options, you can see `:help option-list`
+--
+---- Make line numbers default
+--vim.opt.number = true
+--vim.opt.relativenumber = true
+--
+---- Enable mouse mode, can be useful for resizing splits for example!
+---- vim.opt.mouse = 'a'
+--
+---- Don't show the mode, since it's already in the status line
+--vim.opt.showmode = false
+--
+---- Sync clipboard between OS and Neovim.
+----  Schedule the setting after `UiEnter` because it can increase startup-time.
+----  Remove this option if you want your OS clipboard to remain independent.
+----  See `:help 'clipboard'`
+----vim.schedule(function()
+----  vim.opt.clipboard = 'unnamedplus'
+----end)
+--
+---- Enable break indent
+--vim.opt.breakindent = true
+--
+---- Save undo history
+--vim.opt.undofile = true
+--
+---- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+--vim.opt.ignorecase = true
+--vim.opt.smartcase = true
+--
+---- Keep signcolumn on by default
+--vim.opt.signcolumn = 'yes'
+--
+---- Decrease update time
+--vim.opt.updatetime = 250
+--
+---- Decrease mapped sequence wait time
+--vim.opt.timeoutlen = 300
+--
+---- Configure how new splits should be opened
+--vim.opt.splitright = true
+--vim.opt.splitbelow = true
+--
+---- Sets how neovim will display certain whitespace characters in the editor.
+----  See `:help 'list'`
+----  and `:help 'listchars'`
+--vim.opt.list = true
+--vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+--
+---- Preview substitutions live, as you type!
+--vim.opt.inccommand = 'split'
+--
+---- Show which line your cursor is on
+--vim.opt.cursorline = true
+--
+---- Minimal number of screen lines to keep above and below the cursor.
+--vim.opt.scrolloff = 10
+--
+---- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+---- instead raise a dialog asking if you wish to save the current file(s)
+---- See `:help 'confirm'`
+--vim.opt.confirm = true
+--
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -105,6 +106,9 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Center after going half a page down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center after going half a page up' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
