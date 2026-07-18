7be14ce2b1393da7b2a73374b8273ad7ff97b6fa
JL <115737916+yikesboy@users.noreply.github.com>
2026-07-18 16:24:51 +0200

    feat: add shortened inline text

--- config/plugins/lsp/diagnostics.nix
+++ config/plugins/lsp/diagnostics.nix
@@ -16,7 +16,23 @@
       HINT = "󰌶 ";
     };
 
-    virtual_text = false;
+    virtual_text = {
+      current_line = false;
+      source = "if_many";
+      spacing = 2;
+      format.__raw = ''
+        function(diagnostic)
+          local message = diagnostic.message:gsub("\n", " ")
+          local max_width = math.max(20, vim.api.nvim_win_get_width(0) - 40)
+
+          if #message > max_width then
+            return message:sub(1, max_width - 3) .. "..."
+          end
+
+          return message
+        end
+      '';
+    };
 
     virtual_lines = {
       current_line = true;
